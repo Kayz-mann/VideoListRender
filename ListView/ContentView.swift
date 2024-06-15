86dc69f92f458987c25431596be736be0712a325
Kayode <kayodefredrickbalogun@gmail.com>
2024-06-15 14:43:59 +0100

    first commit

--- ListView/ContentView.swift
+++ ListView/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
+var videos: [Video] = VideoList.topTwelve
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(videos) { video in
+                HStack {
+                    Image(video.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 90.0)
+                        .cornerRadius(8)
+                    
+                    Spacer().frame(width: 16)
+                    
+                    VStack(alignment: .leading) {
+                        Text(video.title)
+                            .fontWeight(.semibold)
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.75)
+                        
+                        Spacer().frame(height: 5)
+                        
+                        Text(video.uploadDate)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    Spacer() // Optional: to push the text to the leading edge
+                }
+                .padding(.vertical, 8) // Optional: Add vertical padding for better spacing
+            }
+            .navigationBarTitle("My Videos")
         }
-        .padding()
     }
 }
 
