a081de58da67faa5caafbbe1855add5916dae7f1
Kayode <kayodefredrickbalogun@gmail.com>
2024-06-15 12:34:59 +0100

    Initial Commit

---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by Balogun Kayode on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
